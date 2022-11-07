cc2ddd661caf14467a31c38f87fb6ee09284bad5
Mati111 <m.chaparrodaz@uandresbello.edu>
2022-11-07 12:36:17 -0300

    Avances en pagina de inicio

---
+++ src/components/menulogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+class MenuLogo extends React.Component{
+  render(){
+    return (
+    <div class= "menuLogo">
+      <Logo />
+    </div>
+    );
+  }
+}
+
+export default MenuLogo;
---
+++ src/components/shortcuts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Shortcuts extends React.Component{
+  render(){
+    return (
+      <div class="shortcut">        
+        <button> Promociones</button>   
+        <button> Menus</button>   
+        <button> Extras</button>
+        <button> Despacho</button>         
+      </div>
+    );
+  }
+}
+
+export default Shortcuts;
