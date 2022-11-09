d2bfac780b75fe84bfb0453e5b435aecf168a23e
matii111 <m.chaparrodaz@uandresbello.edu>
2022-11-09 00:12:18 -0300

    Logo al click devuelve a home

--- src/components/menulogo.js
+++ src/components/menulogo.js
@@ -2,10 +2,9 @@ import React from 'react';
 class MenuLogo extends React.Component{
   render(){
     return (
-    <div>      
-      <img class="menuLogo" alt="logo"
-      src="https://media.discordapp.net/attachments/352829804057329674/1039616691979690117/sushi.png"       
-      />
+    <div>         
+      <a href="/"><img class="menuLogo" alt="logo"
+      src="https://media.discordapp.net/attachments/352829804057329674/1039616691979690117/sushi.png"/></a>            
     </div>
     );
   }
