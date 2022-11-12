8a7fbafdef52fbcaeae488ec6d4857238e5419f0
matii111 <m.chaparrodaz@uandresbello.edu>
2022-11-12 03:46:03 -0300

    Se incorpora el mapa para el despacho

--- src/App.js
+++ src/App.js
@@ -9,29 +9,47 @@ import styled from 'styled-components';
 import Slideshow from './components/slideshow';
 import ItemsMenu, { Items } from './components/productos/itemsMenu';
 import ItemsExtras from './components/productos/itemsExtras';
-
-
+import MapDelivery from './components/map/mapDelivery';
 // TODO:Hay que implementar el coso de maps y las ventanas esas dinamicas de pcfactory, ademas dentro de esas hay que agregar
 // productos y  para luego hacer los popup de cada uno
 class App extends React.Component {
   render() {
     return (
-      <div class="contentBackground">
-        <Navbar></Navbar>
-        <header id="promociones" class="header-image">
-          <SelectRol></SelectRol>
-          <MenuLogo></MenuLogo>
-        </header>
-        <Slideshow></Slideshow>
-        <GotTop></GotTop>
-        <div class="content">
-          <body>
-            <ItemsMenu></ItemsMenu>
-            <ItemsExtras></ItemsExtras>
-          </body>
+      <>
+        <div class="contentBackground">
+          <Navbar></Navbar>
+          <header id="promociones" class="header-image">
+            <SelectRol></SelectRol>
+            <MenuLogo></MenuLogo>
+          </header>
+          <Slideshow></Slideshow>
+          <GotTop></GotTop>
+          <div class="content">
+            <body>
+              <ItemsMenu></ItemsMenu>
+              <ItemsExtras></ItemsExtras>
+              <h1 id="despacho">Despacho</h1>
+              <div style={
+                { 
+                border: "1px", 
+                width: "500px", 
+                height: "300px",
+                margin:"20px",
+                padding:"20px",      
+                paddingLeft:"230px",              
+                zIndex:"1"       
+                }}>                        
+                <MapDelivery />                                
+              </div>
+            </body>
+          </div>
+          <Footer></Footer>
         </div>
-        <Footer></Footer>
-      </div>
+        <div>
+
+        </div>
+
+      </>
     );
   }
 }
